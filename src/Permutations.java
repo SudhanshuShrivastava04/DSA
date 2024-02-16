1316dd881917d8a8ceedc41498d1fa12199b885e
Sudhanshu Shrivastava <211230053@nitdelhi.ac.in>
2024-02-16 15:16:56 +0530

    permutations using recursion backtracking

---
+++ src/Permutations.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutations {
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2, 3};
+        permutation(nums);
+    }
+
+    public static void permutation(int[] nums) {
+        List<List<Integer>> resultlist = new ArrayList<>();
+        generatePermutations(resultlist, nums);
+        resultlist.sort((list1, list2) -> list1.get(0).compareTo(list2.get(0)));
+        System.out.println(resultlist);
+        System.out.println("Total number of permutations:" + resultlist.size());
+    }
+
+    public static void generatePermutations(List<List<Integer>> resultlist, int[] nums) {
+        backtrack(resultlist, new ArrayList<>(), nums);
+    }
+
+    private static void backtrack(List<List<Integer>> resultlist, ArrayList<Integer> templist, int[] nums) {
+        if (templist.size() == nums.length) {
+            resultlist.add(new ArrayList<>(templist));
+            return;
+        }
+
+        for (int number : nums) {
+            if (templist.contains(number))
+                continue;
+            templist.add(number);
+            backtrack(resultlist, templist, nums);
+            templist.remove(templist.size() - 1);
+        }
+    }
+
+    private List<Integer> convertArrayToList(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for (int num : nums) {
+            list.add(num);
+        }
+        return list;
+    }
+}
