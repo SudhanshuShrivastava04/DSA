46328dc4bd33339a23b672b0161869b93731bb52
Sudhanshu Shrivastava <211230053@nitdelhi.ac.in>
2024-03-05 11:56:43 +0530

    Matrix possible moves

---
+++ src/MazePaths.java
@@ -0,0 +1,19 @@
+public class MazePaths {
+    // constraint moves R and D only
+    public static void path(String p, int r , int c){
+        if(r == 1 && c == 1) {
+            System.out.println(p);
+            return;
+        }
+        if(r>1){
+            path(p+"D", r-1, c);
+        }
+        if(c>1){
+            path(p+"R", r, c-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        path("",3,3);
+    }
+}
